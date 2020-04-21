60dccd23c55da871a7aef6d406302dfba307f547
Layik Hama <layik.hama@gmail.com>
2020-04-21 01:32:14 +0100

    Understanding the json

--- phe.R
+++ phe.R
@@ -1,15 +1,17 @@
+library(xml2)
+library(sf)
+library(jsonlite)
+library(magrittr)
+
 xmlURL = 'https://publicdashacc.blob.core.windows.net/publicdata?restype=container&comp=list'
 dataURL = 'https://c19pub.azureedge.net/'
 
 xml = xml2::read_xml(xmlURL)
-library(xml2)
 xml = xml %>% xml_find_all("//Blob") %>% xml_find_all("//Name") %>% 
   xml_text()
 data = xml[grep("data_", xml)]
 geojson = xml[grep(".geojson", xml)]
 
-library(sf)
-library(jsonlite)
 utlas = st_read(paste0(dataURL, xml[grep("utlas", xml)]), stringsAsFactors = F)
 # zipped json from url
 tmp = file.path(tempdir(),"data.json")
@@ -18,17 +20,19 @@ json = readLines(tmp) %>% fromJSON()
 
 json$lastUpdatedAt
 # json$utlas[[1]]$name$value
-addCasesToUTLAs <- function(utlas, json, d) {
+addCasesToUTLAs <- function(utlas, json, d = NA) {
   tmp = utlas
   tmp$totalCases = NA
   for (x in utlas$ctyua19nm) {
     for (y in json$utlas) {
       if(y$name$value == x) {
-        if(exists("d")) {
-          for (v in y$dailyTotalConfirmedCases) {
-            if(v$date == d) {
-              tmp$totalCases[tmp$ctyua19nm == x] =
-                unname(unlist(v$value))
+        if(!is.na(d)) {
+          for (v in y$dailyTotalConfirmedCases$date) {
+            if(v == d) {
+              tmp$totalCases[tmp$ctyua19nm == x] = 
+                y$dailyTotalConfirmedCases[
+                  y$dailyTotalConfirmedCases$date == d, "value"
+                ]
             }
           }
         } else {
@@ -40,7 +44,3 @@ addCasesToUTLAs <- function(utlas, json, d) {
   }
   tmp
 }
-utlasWithCases = addCasesToUTLAs(utlas, json, "2020-03-14")
-# specify date
-plot(utlasWithCases[,"totalCases"])
-
